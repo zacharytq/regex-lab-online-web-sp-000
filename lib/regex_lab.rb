5e6cfe33732eaa4cf18c724d6eae9c78d93dc512
chic-page-6502 <zacharytq@gmail.com>
2019-07-12 01:21:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,4 @@
 def starts_with_a_vowel?(word)
-# (/[aeiou]/)
-
   if word.match(/\A[aeiou]/i) == nil
     return false
   else
@@ -13,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
