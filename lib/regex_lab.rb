32a4d9ee1f591c13dfba89dcd7768e1c0d9672ad
chic-page-6502 <zacharytq@gmail.com>
2019-07-12 01:17:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
 def starts_with_a_vowel?(word)
-if word.match(/^[aeiou]\w/)
-  true
-else
-  nil
-end
+# (/[aeiou]/)
+
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
