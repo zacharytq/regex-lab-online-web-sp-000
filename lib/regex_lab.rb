1e5fde698993b2fe11de9e0a3c2887eaa359c924
chic-page-6502 <zacharytq@gmail.com>
2019-07-12 01:22:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w{5}/)
+  text.scan(/\b\w{5}/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
